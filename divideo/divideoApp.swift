f66a6c6458f17f9b6217b12bd27cec0a55b4db7a
Rodrigo Bueno <rodrigo.bueno@wdtl.com>
2023-09-30 15:29:08 +1300

    Initial Commit

---
+++ divideo/divideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  divideoApp.swift
+//  divideo
+//
+//  Created by Rodrigo Bueno on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct divideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
